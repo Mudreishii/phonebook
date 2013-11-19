b2c3885c51739aadf92ae7bb431b7761741a54b0
Mudreishii <Mudreishii@yandex.ru>
2013-11-19 11:54:55 +0200

    redact contact

--- phonebook.js
+++ phonebook.js
@@ -1,123 +1,137 @@
 $(document).ready(function () {
-	
 
-var contacts = [
-	{
-		firstName:"Дима",
-		lastName:"Белов",
-		operator:"063",
-		phoneNumber:"0631346611"
-	},
-	{
-		firstName:"Александр",
-		lastName:"Соловей",
-		operator:"067",
-		phoneNumber:"911"
-	}
-];
+    var contacts = [{
+        firstName: "Дима",
+        lastName: "Белов",
+        operator: "063",
+        phoneNumber: "0631346611"
+    }, {
+        firstName: "Александр",
+        lastName: "Соловей",
+        operator: "067",
+        phoneNumber: "911"
+    }];
 
-// =============================================
-var pokazForm = 0; // Счётчик открытия и закрытия формы
+    var pokazForm = 0; // Счётчик открытия и закрытия формы
 
-// Добовление контакта
-add ({
-firstName: "John",
-lastName: "Resiz",
-operator: "066",
-phoneNumber: "098239146"
-});
+    // Добовление контакта
+    add({
+        firstName: "John",
+        lastName: "Resiz",
+        operator: "066",
+        phoneNumber: "098239146"
+    });
 
-function add (newContact) {
-	contacts.push(newContact);
-}
+    function add(newContact) {
+        contacts.push(newContact);
+    }
 
-// Вывод контактов
-function vivodContacts () {
-	$.each(contacts, function (index, contact) {
-	
-		var blockKnopok = $("<div>");
-		var buttonEdit = $("<button class='btn btn-success success'>").text('Edit');
-		var buttonDelit = $("<button class='btn btn-danger danger' style='margin-left:5px'>").text('Delete');
-		blockKnopok.append(buttonEdit);
-		blockKnopok.append(buttonDelit);
+    // Вывод контактов
+    function vivodContacts() {
+        $('.table-contacts .table tbody').find($('tr')).remove();
+        $.each(contacts, function (index, contact) {
 
+            var blockKnopok = $("<div>");
+            var buttonEdit = $("<button class='btn btn-success success'>").text('Edit');
+            var buttonDelit = $("<button class='btn btn-danger danger' style='margin-left:5px'>").text('Delete');
+            blockKnopok.append(buttonEdit);
+            blockKnopok.append(buttonDelit);
 
-		var tr = $("<tr>");
-		var tdFirstName = $("<td>").text(contact.firstName);
-		var tdLastName = $("<td>").text(contact.lastName);
-		var tdOperator = $("<td>").text(contact.operator);
-		var tdPhoneNumber = $("<td>").addClass("tdPhon").text(contact.phoneNumber);
-		var tdAction = $("<td>").html(blockKnopok);
+            var tr = $("<tr>");
+            var tdFirstName = $("<td>").text(contact.firstName);
+            var tdLastName = $("<td>").text(contact.lastName);
+            var tdOperator = $("<td>").text(contact.operator);
+            var tdPhoneNumber = $("<td>").addClass("tdPhon").text(contact.phoneNumber);
+            var tdAction = $("<td>").html(blockKnopok);
 
-		tr.append(tdFirstName);
-		tr.append(tdLastName);
-		tr.append(tdOperator);
-		tr.append(tdPhoneNumber);
-		tr.append(tdAction);
+            tr.append(tdFirstName);
+            tr.append(tdLastName);
+            tr.append(tdOperator);
+            tr.append(tdPhoneNumber);
+            tr.append(tdAction);
 
-		$('.table-contacts tbody').append(tr);
-	})
-	
-}
-vivodContacts();
+            $('.table-contacts tbody').append(tr);
+        })
 
+    }
+    vivodContacts();
 
-// фывфывфвфвы
-// Открытие и закрытие формы добовления контакта
-// Открытие
-$('.add-contact').click(function  () {
-	if (pokazForm==0) {
-		var formAddContact = $('#edd-block-form-horizontal').clone();
-		var blockFormAddContact = $('<div>').addClass("blockFormAddContact");
+    // Открытие и закрытие формы добовления контакта
+    // Открытие
+    $('.add-contact').click(function () {
+        if (pokazForm == 0) {
+            var formAddContact = $('#edd-block-form-horizontal').clone();
+            var blockFormAddContact = $('<div>').addClass("blockFormAddContact");
 
-		$('body').append(blockFormAddContact);
-		blockFormAddContact.append(formAddContact);
-		blockFormAddContact.animate({
-			'marginTop': 150
-		})
-		formAddContact.fadeIn();
-		pokazForm=1;
-	};
-})
-// Закрытие
-$('body').on('click','.button-krest',function () {
-	$(".blockFormAddContact").remove();
-	pokazForm=0;
-})
+            $('body').append(blockFormAddContact);
+            blockFormAddContact.append(formAddContact);
+            blockFormAddContact.animate({
+                'marginTop': 150
+            })
+            formAddContact.fadeIn();
+            pokazForm = 1;
+        };
+    })
+    // Открытие и закрытие формы редактирования контакта
+    // Открытие
+    $('body').on('click', '.table-contacts .table .success', function () {
+        var formRedact = $('#block-form-horizontal-redact').clone();
+        var blockFormRedact = $('<div>').addClass("blockFormRedact");
 
-
-// Удоление контакта
-
-// $('.table-contacts .danger').click(function (event) {
-// 	var a = $(event.currentTarget);
-// 	var b = a.closest('tr').find('.tdPhon').text();
-// 	console.log(b);
-// 	for (var i = 0; i < contacts.length; i++) {
-// 		if (contacts[i]['phoneNumber']==b) {
-// 			contacts.splice(i);
-// 			console.log(contacts);
-// 			// console.log(contacts[i]['firstName']);
-// 		};
-// 	};
-// })
+        $('body').append(blockFormRedact);
+        blockFormRedact.append(formRedact);
+        blockFormRedact.animate({
+            'marginTop': 150
+        })
+        formRedact.fadeIn();
+        pokazForm = 1;
+    })
+    // Закрытие
+    $('body').on('click', '.button-krest', function () {
+        $(".blockFormAddContact, .blockFormRedact").remove();
+        pokazForm = 0;
+    })
 
 
-// var z = 911;
+    // Удоление контакта
+    function remove(telephone) {
+        var indexDelit;
+        $.each(contacts, function (index, mas) {
+            if (mas.phoneNumber == telephone) {
+                indexDelit = index;
+            };
+        })
+        contacts.splice(indexDelit, 1);
+        vivodContacts();
+    }
+    $('body').on('click', '.table-contacts .danger', function (e) {
+        var target = $(e.currentTarget);
+        var phoneNumber = target.closest('tr').find('.tdPhon').text();
+        remove(phoneNumber);
+    })
 
-function remove (telephone) {
-	var indexDelit;
-	$.each(contacts, function (index, mas) {
-		if (mas.phoneNumber==telephone) {
-			indexDelit=index;
-			alert(indexDelit);
-			console.log(contacts);
-		};
-	})
-	contacts.splice(indexDelit,1);
-	console.log(contacts);
-}
-remove(911);
 
+    // Редактирование контакта
+    var oldPhoneNumber = 911;
+    var newFirstName = 8;
+    var newLastName = 7;
+    var newOperator = 6;
+    var newPhoneNumber = 5;
+
+    function redactContact(oldPhoneNumber, newFirstName, newLastName, newOperator, newPhoneNumber) {
+        $.each(contacts, function (index, mas) {
+            if (mas.phoneNumber == oldPhoneNumber) {
+                contacts[index]['firstName'] = newFirstName;
+                contacts[index]['lastName'] = newLastName;
+                contacts[index]['operator'] = newOperator;
+                contacts[index]['phoneNumber'] = newPhoneNumber;
+            };
+        })
+        vivodContacts();
+    }
+    $('body').click(function (argument) {
+        redactContact(oldPhoneNumber, newFirstName, newLastName, newOperator, newPhoneNumber);
+    })
 
 
 
